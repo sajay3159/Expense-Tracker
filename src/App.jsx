d63abaf4e4aa579fd186162d22037be477f08b7c
Ajay Sahani <sajay3159@gmail.com>
2025-10-08 09:15:30 +0530

    Login Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import LoginForm from "./components/Auth/LoginForm";
 import SignupForm from "./components/Auth/SignupForm";
 import Header from "./components/Header";
 
 function App() {
+  const Welcome = () => (
+    <h2 className="text-center">Welcome to Expense tracker</h2>
+  );
   return (
     <>
       <Header />
-      <SignupForm />
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/signup" component={SignupForm} />
+        <Route path="/login" component={LoginForm} />
+      </Switch>
     </>
   );
 }
--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,11 +1,18 @@
 import { useRef, useState } from "react";
-import { Button, Card, Container, FloatingLabel } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Container,
+  FloatingLabel,
+  NavLink,
+} from "react-bootstrap";
 import Form from "react-bootstrap/Form";
+import { useHistory } from "react-router-dom";
 
-const SignupForm = () => {
+const LoginForm = () => {
+  const history = useHistory();
   const emailRef = useRef();
   const passwordRef = useRef();
-  const confirmPasswordRef = useRef();
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
 
@@ -14,18 +21,12 @@ const SignupForm = () => {
 
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-    const confirmPassword = confirmPasswordRef.current.value;
-
-    if (password !== confirmPassword) {
-      setError("Passwords do not match.");
-      return;
-    }
 
     const apiKey = "AIzaSyDpWVsvC9evJbXOQnZHUyAxGQIOfLTaZOs";
 
     try {
       const response = await fetch(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`,
         {
           method: "POST",
           body: JSON.stringify({
@@ -42,26 +43,27 @@ const SignupForm = () => {
       const data = await response.json();
 
       if (!response.ok) {
+        alert("wrong email and password");
         throw new Error(data.error.message || "Something went wrong");
       }
 
-      setSuccess("Signup successful!");
+      setSuccess("Login successful!");
       setError("");
-      console.log("Signup successful");
+      history.push("/");
+      localStorage.setItem("token", data.idToken);
     } catch (err) {
       setError(err.message);
       setSuccess("");
     }
     emailRef.current.value = "";
     passwordRef.current.value = "";
-    confirmPasswordRef.current.value = "";
   };
 
   return (
     <Container className="my-5" style={{ maxWidth: "400px" }}>
       <Card style={{ width: "20rem" }} className="py-3 mb-3">
         <Card.Body>
-          <Card.Title className="text-center py-4">Sign Up</Card.Title>
+          <Card.Title className="text-center py-4">Login</Card.Title>
           <Form onSubmit={handleSubmit}>
             <FloatingLabel label="Email address" className="mb-3">
               <Form.Control
@@ -78,15 +80,6 @@ const SignupForm = () => {
                 ref={passwordRef}
               />
             </FloatingLabel>
-
-            <FloatingLabel label="Confirm Password" className="mb-3">
-              <Form.Control
-                type="password"
-                placeholder="Confirm Password"
-                ref={confirmPasswordRef}
-              />
-            </FloatingLabel>
-
             {error && <p className="text-danger text-center">{error}</p>}
             {success && <p className="text-success text-center">{success}</p>}
 
@@ -96,8 +89,11 @@ const SignupForm = () => {
                 variant="primary"
                 style={{ borderRadius: "20px" }}
               >
-                Sign Up
+                Login
               </Button>
+              <NavLink className="text-center text-primary text-decoration-underline">
+                Forget Password
+              </NavLink>
             </div>
           </Form>
         </Card.Body>
@@ -107,10 +103,12 @@ const SignupForm = () => {
         style={{ width: "20rem", backgroundColor: "#c3dbcf" }}
         className="py-2"
       >
-        <Card.Body className="text-center">Have an account? Login</Card.Body>
+        <Card.Body className="text-center">
+          Don't have an account? Sign up
+        </Card.Body>
       </Card>
     </Container>
   );
 };
 
-export default SignupForm;
+export default LoginForm;
