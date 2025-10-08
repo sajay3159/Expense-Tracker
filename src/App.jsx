d79e9f92aa53a039369cfee01e01502981fdf8d9
Ajay Sahani <sajay3159@gmail.com>
2025-10-08 21:22:26 +0530

    fix security issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 import LoginForm from "./components/Auth/LoginForm";
 import SignupForm from "./components/Auth/SignupForm";
 import Header from "./components/Header";
 import ProfilePage from "./pages/ProfilePage";
 import ProfileForm from "./components/Profile/ProfileForm";
+import { useContext } from "react";
+import AuthContext from "./store/auth-context";
+import NotFound from "./components/NotFound";
 
 function App() {
+  const authCtx = useContext(AuthContext);
+
   return (
     <>
       <Header />
       <Switch>
         <Route path="/" exact component={SignupForm} />
-        <Route path="/profile" component={ProfilePage} />
-        <Route path="/profile-form" component={ProfileForm} />
+        {authCtx.isLoggedIn && (
+          <>
+            <Route path="/profile" component={ProfilePage} />
+            <Route path="/profile-form" component={ProfileForm} />
+          </>
+        )}
+        {!authCtx.isLoggedIn && (
+          <Route path="/profile" render={() => <Redirect to="/login" />} />
+        )}
         <Route path="/login" component={LoginForm} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </>
   );
